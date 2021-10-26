63203cff5e3eb9498f956228d7705815dda977cf
justpwned <justpwnedfrom@gmail.com>
2021-10-27 03:11:19 +0500

    add ci lint and test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,7 @@
 from gendiff import generate_diff
 import json
 
+
 def test_diff():
     file1 = json.load(open('tests/fixtures/file1.json'))
     file2 = json.load(open('tests/fixtures/file2.json'))
